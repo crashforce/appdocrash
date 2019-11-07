64deb6c0fe281a0f8d65a4fe3925a6adad351eca
CrashForce <crashforce@gmail.com>
2019-11-06 20:27:45 -0400

    Commit com os arquivos, bases do app.

---
+++ config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'israel-wilson-app',
+		wp_ws_url : 'http://app.israelwilson.com.br/wp-appkit-api/israel-wilson-app',
+		wp_url : 'http://app.israelwilson.com.br',
+		theme : 'q-ios',
+		version : '',
+		app_type : 'phonegap-build',
+		app_title : 'Israel Wilson APP',
+		app_platform : 'ios',
+		app_path: '',
+		gmt_offset : -2,
+		debug_mode : 'off',
+		auth_key : 'o@lUIS&zI>H%v&E<EIOd+%lj+xtC3I.|OAO*mqQCA{}2F)0d%Hq6l.+j1#|_8p8N',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
